3b85d0c306300746645e0e1fc84de9548e0a56f4
eminshamshoian <eminshamshoian@gmail.com>
2022-01-31 15:57:39 -0800

    created button, forminput and radio input reusable components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,36 +2,27 @@ import React from 'react';
 // useHistory is for routes accessed before
 import { Link } from 'react-router-dom';
 
-
-const Navbar = props => {
-
-    
-
-    return (
-        <>
-
-                <div className='navbar'>
-                    <div className='container'>
-                        <Link to={'/dashboard'} className='navbar-brand'>
-                            Facility App
-                        </Link>
-                        <div className='collapses navbar-collapses'>
-                            <ul className='navbar-nav ml-auto'>
-                                <li className='nav-item'>
-                                    <a href='#!' >
-                                        <i className='fas fa-sign-out-alt'></i>{' '}
-                                        <span>Logout</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            
-        </>
-    );
+const Navbar = (props) => {
+  return (
+    <>
+      <div className='navbar'>
+        <div className='container'>
+          <Link to={'/dashboard'} className='navbar-brand'>
+            Facility App
+          </Link>
+          <div className='collapses navbar-collapses'>
+            <ul className='navbar-nav ml-auto'>
+              <li className='nav-item'>
+                <a href='#!'>
+                  <i className='fas fa-sign-out-alt'></i> <span>Logout</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
-
-
 
 export default Navbar;
---
+++ src/components/reusable/RadioInput.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const RadioInput = (props) => {
+  const { id, name, onChange, className, value, error, labelClassName } = props;
+  return (
+    <>
+      <input
+        className={className}
+        type='radio'
+        name={name}
+        id={id}
+        value={value}
+        autoComplete='false'
+        onChange={onChange}
+      />
+      <label
+        style={{
+          color: error ? 'red' : '#36404a',
+          fontWeight: 'normal',
+        }}
+        className={labelClassName}
+        htmlFor={id}
+      >
+        {value}
+      </label>
+    </>
+  );
+};
+
+// Like a form of validation
+RadioInput.prototypes = {
+  id: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  value: PropTypes.any,
+  onChange: PropTypes.func.isRequired,
+  error: PropTypes.string,
+  labelClassName: PropTypes.string,
+};
+
+export { RadioInput };
