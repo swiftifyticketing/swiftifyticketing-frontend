66a5b0c76e5d65eca1389a259a434a42bb8c1ca0
cobra11793 <vmakdesian@gmail.com>
2022-04-16 22:04:02 -0700

    added home page, navigation bar, icon. fixed links but still needs to fix for logged in user.

--- src/components/auth/Home.js
+++ src/components/auth/Home.js
@@ -1,12 +1,10 @@
 import React from "react";
 import HeroSection from "../layout/HeroSection";
-import Navbar from "../layout/Navbar";
 
 
 function Home(){
     return(
         <>
-        <Navbar />
         <HeroSection />
         </>
     );
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 // useHistory is for routes accessed before
 import { Link, useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import './Navbar.css';
 
 
 import { logout } from '../../redux/actions/auth';
@@ -10,11 +11,31 @@ import { logout } from '../../redux/actions/auth';
 const Navbar = props => {
     const { isAuthenticated, logout } = props;
     const history = useHistory();
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const closeMobileMenu = () => setClick(false);
+    const handleClick = () => setClick(!click);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+
+    };
 
     const logoutUser = () => {
         logout();
         history.push('/');
     };
+
+    useEffect(() => {
+        showButton();
+    }, []);
+    window.addEventListener('resize', showButton);
+
 
     return (
         <>
@@ -37,7 +58,44 @@ const Navbar = props => {
                     </div>
                 </div>
             ) : (
-                ''
+                <nav className='navbar'>
+                    <div className='navbar-container'>
+                        <Link to={'/'} className='navbar-logo'>
+                            <img className='icon' alt='logo' src='public\..\SwiftifyTicketingIcon.png' height={80} width={175} />
+                        </Link>
+                        <div className='menu-icon' onClick={handleClick}>
+                            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                        </div>
+                        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                            <li className='nav-item'>
+                                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                    Home
+                                </Link>
+                            </li>
+                            <li className='nav-item'>
+                            </li>
+                            <li className='nav-item'>
+                                <Link
+                                    to='/sign-in'
+                                    className='nav-links'
+                                    onClick={closeMobileMenu}
+                                >
+                                    Login
+                                </Link>
+                            </li>
+
+                            <li>
+                                <Link
+                                    to='/sign-up'
+                                    className='nav-links'
+                                    onClick={closeMobileMenu}
+                                >
+                                    Sign Up
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
             )}
         </>
     );
