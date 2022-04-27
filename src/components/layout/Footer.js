70bc1fa81f9d4d2dd776137bac9626f0e3e0404a
cobra11793 <vmakdesian@gmail.com>
2022-04-26 18:15:23 -0700

    Finished Footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,51 +5,47 @@ import './Footer.css';
 function Footer() {
     return (
         <section className="footer">
-        <hr className="footer-seperator" />
-        <section className="footer-social-media">
-        <img className='footer-social-media__icon' alt='logo' src='public\..\STIcon.png' />
-        
-        </section>
-        
-        
-        <section className="footer-info">
-          <section className="footer-info-left">
-          <h4 className='swift'>Contact Us</h4> 
-            <section className="footer-info__name">
-            
-            <a className="fas fa-envelope-square" href="mailto:varoujan.makdesian.314@my.csun.edu"></a> |
-            <a className="fab fa-linkedin"href="https://www.linkedin.com/in/varoujan-makdesian-9b5857187/"></a> |
-            <a href="https://www.facebook.com/SwiftifyTicketing-112132488145860" className="fab fa-facebook-square"></a> 
+            <hr className="footer-seperator" />
+            <section className="footer-social-media">
+                
             </section>
-            <section className="footer-info__returns">
+            <section className="footer-info">
+                <section className="footer-info-left">
+                <img className='footer-social-media__icon' alt='logo' src='public\..\STIcon.png' />
+                
+                    <section className="footer-info__name">
+                    </section>
+                    <section className="footer-info__returns">
+                    </section>
+                </section>
+                <section className="footer-info-center">
+               
+                    <section className="footer-info__terms">
+                    <h4 className='swift'>SwiftifyTicketing</h4>
+                    Terms and Conditions 
+                        <br></br>
+                        Copyright 2022 
+                        <br></br>
+                        All rights reserved
+                    </section>
+                </section>
+                <section className="footer-info-right">
+                    <section className="footer-info__number">
+                        <h4 className='swift'> Contact Us:
+                            <a className="fas fa-envelope-square" href="mailto:swiftifyticketing@gmail.com"></a> |
+                            <a className="fab fa-github" href="https://github.com/swiftifyticketing"></a> |
+                            <a href="https://www.facebook.com/SwiftifyTicketing-112132488145860" className="fab fa-facebook-square"></a>
+                        </h4>
+                    </section>
+                    <section className="footer-info__contact">
 
-              <br />
-              
-            </section>        
-          </section>
-          <section className="footer-info-center">
-            <h4 className='swift'>SwiftifyTicketing</h4>
-            <section className="footer-info__terms">
-              Terms and Conditions
-              <br />
-              Copyright 2022
-              <br />
-              All rights reserved
+                        <br />
+
+                    </section>
+                </section>
             </section>
-          </section>
-          <section className="footer-info-right">
-            <section className="footer-info__number">
-              
-            </section>
-            <section className="footer-info__contact">
-           
-              <br />
-              
-            </section>
-          </section>
+            <hr className="footer-seperator" />
         </section>
-        <hr className="footer-seperator" />
-      </section>
     )
 }
 
