12bfda178ae0ffd8aff8c679bf4a142744559795
Emin <eminshamshoian@gmail.com>
2022-06-27 16:15:14 -0700

    added sample login info

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,121 +1,117 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-import { FormInput } from '../reusable/FormInput';
-import { Button } from '../reusable/Button';
-import { validateInputs } from '../../helpers/Helpers';
-import { loginUser } from '../../redux/actions/auth';
-import './Auth.css';
+import { FormInput } from "../reusable/FormInput";
+import { Button } from "../reusable/Button";
+import { validateInputs } from "../../helpers/Helpers";
+import { loginUser } from "../../redux/actions/auth";
+import "./Auth.css";
 
-import './Auth.css';
+import "./Auth.css";
 
-const Login = props => {
-    const { loginUser, isAuthenticated, history, errors } = props;
+const Login = (props) => {
+  const { loginUser, isAuthenticated, history, errors } = props;
 
-    const [user, setUser] = useState({
-        data: {
-            username: '',
-            password: ''
-        }
+  const [user, setUser] = useState({
+    data: {
+      username: "admin",
+      password: "admin1234",
+    },
+  });
+
+  const [error, setError] = useState({
+    usernameError: "",
+    passwordError: "",
+  });
+
+  const { username, password } = user.data;
+  const { usernameError, passwordError } = error;
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      history.push("/dashboard");
+    }
+  }, [isAuthenticated, history]);
+
+  const onLoginUser = (e) => {
+    e.preventDefault();
+
+    const isValid = validateInputs(user.data, setError);
+
+    if (isValid) {
+      loginUser(user.data);
+    }
+  };
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    const { data } = user;
+    setUser({
+      data: {
+        ...data,
+        [name]: value,
+      },
     });
+  };
 
-    const [error, setError] = useState({
-        usernameError: '',
-        passwordError: ''
-    });
+  return (
+    <div className='auth-wrapper'>
+      <div className='auth-inner'>
+        <form onSubmit={onLoginUser}>
+          <h3>Sign In</h3>
 
-    const { username, password } = user.data;
-    const { usernameError, passwordError } = error;
+          <div className='form-group'>
+            <FormInput
+              type='text'
+              name='username'
+              label='Username'
+              className='form-control'
+              placeholder='Enter Username'
+              value={username}
+              error={usernameError}
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <FormInput
+              type='password'
+              name='password'
+              label='Password'
+              className='form-control'
+              placeholder='Enter Password'
+              value={password}
+              error={passwordError}
+              onChange={onChange}
+            />
+          </div>
 
-    useEffect(() => {
-        if (isAuthenticated) {
-            history.push('/dashboard');
-        }
-    }, [isAuthenticated, history]);
+          <Button
+            type='submit'
+            label='Sign In'
+            className='btn btn-primary btn-block'
+          />
+          <p className='forgot-password text-right'>
+            Not yet registered? <Link to={"/sign-up"}>Register</Link>
+          </p>
+        </form>
 
-    const onLoginUser = e => {
-        e.preventDefault();
-
-        const isValid = validateInputs(user.data, setError);
-
-        if (isValid) {
-            loginUser(user.data);
-        }
-    };
-
-    const onChange = e => {
-        const { name, value } = e.target;
-        const { data } = user;
-        setUser({
-            data: {
-                ...data,
-                [name]: value
-            }
-        });
-    };
-
-    return (
-        <div className='auth-wrapper'>
-            <div className='auth-inner'>
-                <form onSubmit={onLoginUser}>
-                    <h3>Sign In</h3>
-
-                    <div className='form-group'>
-                        <FormInput
-                            type='text'
-                            name='username'
-                            label='Username'
-                            className='form-control'
-                            placeholder='Enter Username'
-                            value={username}
-                            error={usernameError}
-                            onChange={onChange}
-                        />
-                    </div>
-                    <div className='form-group'>
-                        <FormInput
-                            type='password'
-                            name='password'
-                            label='Password'
-                            className='form-control'
-                            placeholder='Enter Password'
-                            value={password}
-                            error={passwordError}
-                            onChange={onChange}
-                        />
-                    </div>
-
-                    <Button
-                        type='submit'
-                        label='Sign In'
-                        className='btn btn-primary btn-block'
-                    />
-                    <p className='forgot-password text-right'>
-                        Not yet registered?{' '}
-                        <Link to={'/sign-up'}>Register</Link>
-                    </p>
-                </form>
-
-                {errors ? <p className='error-feedback'>{errors}</p> : ''}
-            </div>
-        </div>
-    );
+        {errors ? <p className='error-feedback'>{errors}</p> : ""}
+      </div>
+    </div>
+  );
 };
 
 Login.propTypes = {
-    loginUser: PropTypes.func.isRequired,
-    isAuthenticated: PropTypes.bool,
-    errors: PropTypes.string
+  loginUser: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
+  errors: PropTypes.string,
 };
 
-const mapStateToProps = state => ({
-    isAuthenticated: state.auth.isAuthenticated,
-    errors: state.errors
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+  errors: state.errors,
 });
 
-export default connect(
-    mapStateToProps,
-    { loginUser }
-)(Login);
+export default connect(mapStateToProps, { loginUser })(Login);
